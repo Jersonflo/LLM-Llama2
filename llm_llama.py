fcb27ea6d118845ef2c4290727d1b4581332c8a3
EnigmaEcho <jgiraldof@unal.edu.co>
2024-10-08 10:37:52 -0500

    Update llm_llama.py

--- llm_llama.py
+++ llm_llama.py
@@ -27,7 +27,7 @@ with st.sidebar:
         llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
     temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
     top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
-    max_length = st.sidebar.slider('max_length', min_value=20, max_value=80, value=120, step=8)
+    max_length = st.sidebar.slider('max_length', min_value=20, max_value=80, value=80, step=8)
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
@@ -44,16 +44,18 @@ st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
 # Function for generating LLaMA2 response. Refactored from https://github.com/a16z-infra/llama2-chatbot
 def generate_llama2_response(prompt_input):
-    string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
-    for dict_message in st.session_state.messages:
+    # Limitar el número de mensajes anteriores enviados al modelo
+    recent_messages = st.session_state.messages[-4:]  # Ajustar 
+    string_dialogue = "Eres un asistente útil y hablas en español. No respondas como 'User'."
+    for dict_message in recent_messages:
         if dict_message["role"] == "user":
             string_dialogue += "User: " + dict_message["content"] + "\n\n"
         else:
             string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
-    output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
-                           input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
-                                  "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1})
+    output = replicate.run(llm, input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
+                                       "temperature": temperature, "top_p": top_p, "max_length": max_length, "repetition_penalty": 1})
     return output
+
 
 # User-provided prompt
 if prompt := st.chat_input(disabled=not replicate_api):
